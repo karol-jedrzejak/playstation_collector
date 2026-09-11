d5010a68a6e3b68d0192d666315543fc38964705
Karol Jędrzejak <karol.jedrzejak@gmail.com>
2026-09-11 10:24:33 +0200

    update auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
+
 @Module({
   imports: [
     PassportModule,
--- backend/src/auth/guard/jwt-auth.guard.ts
+++ backend/src/auth/guard/jwt-auth.guard.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
+// src/auth/guard/jwt-auth.guard.ts
+import {Injectable,} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
